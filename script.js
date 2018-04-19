99c502f8e32cbf7652ebc41077fbec4bcfe0c739
DESKTOP-8B2DGJ1\Timon Bestebreur <timonbestebreur@gmail.com>
2018-04-19 16:40:17 +0200

    added JSON and server.js file for sending JSON

--- script.js
+++ script.js
@@ -6,5 +6,9 @@ var main = function(){
     $("button[id=jokeBtn]").on("click", function () {
         alert("Yo mama so poor she has only one shoe and when someone asks her did you lose a shoe she says no I found one");
     });
+
+    $.getJSON("jokes.json", function(json) {
+        console.log(json); // this will show the info it in firebug console
+    });
 }
 $(document).ready(main);
